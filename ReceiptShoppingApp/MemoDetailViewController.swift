eb1e9c88866c119128c99b992e788810ffe1b225
Ishii-Nozomi <xx2468.egg@gmail.com>
2023-10-23 13:42:34 +0900

    delete処理の書き換えによる保存ボタンのクラッシュ回避の処理

--- ReceiptShoppingApp/MemoDetailViewController.swift
+++ ReceiptShoppingApp/MemoDetailViewController.swift
@@ -173,17 +173,11 @@ extension MemoDetailViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if let existingMemoList = realm.objects(MemoTitleModel.self).first(where: { $0.id == titleRecord.id }) {
-            try! realm.write {
-                realm.delete(existingMemoList)
-            }
-            array.remove(at: indexPath.row)
-            footerView.totalTextLabel.text = String(totalPrice())
-            tableView.reloadData()
-        } else {
             array.remove(at: indexPath.row)
             footerView.totalTextLabel.text = String(totalPrice())
             tableView.reloadData()
         }
+        
     }
     
     func saveRecord(with: MemoTitleModel) {
